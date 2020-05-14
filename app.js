4151a79bca9b1b8792a825d0f8eb82fdc838a69d
Feoh258258 <65220078+Feoh258258@users.noreply.github.com>
2020-05-14 20:01:09 +0300

    HW-02

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-// //   //task1
-
-
-
-
-
-  function validateForm() {
+function validateForm() {
   let x = document.forms["myForm"]["fname"].value;
   let y = document.forms["myForm"]["fname1"].value;
   let z = document.forms["myForm"]["fname2"].value;
@@ -53,19 +47,133 @@ function validateForm2() {
 
 
 //task3
-function validateForm3() {
-  let x = document.forms["myForm3"]["fname12"].value;
-  for (let i = 100000; i <= 999999; i++) {
-    if (isLucky(i)) {
-      console.log(i);
+document.addEventListener("DOMContentLoaded", function() {
+
+  const happyBtn = document.getElementById('happyBtn');
+  happyBtn.onclick = function() {
+    const yourNum = document.getElementById('yourNum').value;
+    numToStr = String(yourNum);
+    firstTreeSymbols = numToStr.substring(0, 3);
+    lastTreeSymbols = numToStr.substring(3, 6);
+    console.log(lastTreeSymbols);
+    if(numToStr.length == 6) {
+      firstPartSymbols = firstTreeSymbols;
+      secondPartSymbols = lastTreeSymbols;
+      let firstPart = 0;
+      let secondPart = 0;
+      for (i = 0; i<firstPartSymbols.lenght; i++){
+        firstPart += parseInt(firstPartSymbols[i]);
+        secondPart += parseInt(secondPartSymbols[i]);
+      }
+      if(firstPart == secondPart){
+        alert('Yes');
+      }else{
+        alert('No');
+      }
+    }
+    else {
+      alert('Введите 6-ти значное число');
     }
   }
   
-  function isLucky(num) {
-    let str = String(num);
-	
-    let sum1 = Number(str[0]) + Number(str[1]) + Number(str[2]);
-    let sum2 = Number(str[3]) + Number(str[4]) + Number(str[5]);
+  const ageBtn = document.getElementById('ageBtn');
+  ageBtn.onclick = function() {
+    const enterAge = document.getElementById('enterAge').value;
+    console.log(enterAge);
+    if(enterAge <= 1, enterAge <=12){
+      alert('ребенок');
+    }else if(enterAge<= 13, enterAge<=18){
+      alert('подросток');
+    }else if(enterAge <= 19, enterAge <= 60){
+      alert('взрослый');
+    }else{
+      alert('пенсионер');
+    }
     
-    return sum1 == sum2;  }
-}
+  }  
+  //закончить с таском!!!
+
+  const oneToNineBtn = document.getElementById('oneToNineBtn')
+  oneToNineBtn.onclick = function() {
+    const enterNum = document.getElementById('enterNum').value;
+    enterNumRewriteToNum = parseInt(enterNum);
+    console.log("NUM:"+enterNumRewriteToNum);
+    switch (enterNumRewriteToNum){
+      case 0:
+      alert(')');
+      break;
+
+      case 1:
+      alert('!');
+      break;
+      
+      case 2:
+      alert('"');
+      break; 
+
+      case 3:
+      alert('№');
+      break;
+      
+      case 4:
+      alert('%');
+      break; 
+
+      case 5:
+      alert(':');
+      break;
+      
+      case 6:
+      alert(',');
+      break; 
+      case 7:
+      alert('.');
+      break;
+      
+      case 8:
+      alert(';');
+      break; 
+
+      case 9:
+      alert('(');
+      break; 
+      
+      default:
+      alert( "Введите число от 1 до 9!" );
+    };
+  }
+  const yearBtn = document.getElementById('yearBtn');
+  yearBtn.onclick = function() {
+    const yearNum = document.getElementById('yearNum').value;
+    let year = yearNum;
+    if(year%400 && !year%100 || year%4){
+      alert("Не высокосный");
+    }else{
+    alert("Высокосный");}
+
+  };
+  
+  
+  const dollarBtn = document.getElementById('dollarBtn');
+  dollarBtn.onclick = function() {
+    let select = document.getElementById('selectCur');
+    let input = document.getElementById('dollarNum').value;
+    input = parseInt(input);
+    let converted = document.getElementById('converted');
+    const rates = {
+      eur: 1.09,
+      uah: 27.99,
+      azn: 1.7
+    };
+    converted = input * rates[select.value];
+    alert((converted.toFixed(2)));
+  };
+
+
+
+
+
+
+
+
+});//End DOMContentLoaded
